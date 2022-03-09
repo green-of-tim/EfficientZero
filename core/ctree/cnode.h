e802412aea25fc112e041817e085b0f84c18395b
green-of-tim <62169271+green-of-tim@users.noreply.github.com>
2022-03-09 19:45:20 +0300

    Update cnode.h

--- core/ctree/cnode.h
+++ core/ctree/cnode.h
@@ -29,6 +29,7 @@ namespace tree {
             void expand(int to_play, int hidden_state_index_x, int hidden_state_index_y, float value_prefix, const std::vector<float> &policy_logits);
             void add_exploration_noise(float exploration_fraction, const std::vector<float> &noises);
             float get_mean_q(int isRoot, float parent_q, float discount);
+            float update_w(int isRoot, float parent_q, float discount);
             void print_out();
 
             int expanded();
@@ -77,9 +78,9 @@ namespace tree {
     void update_tree_q(CNode* root, tools::CMinMaxStats &min_max_stats, float discount);
     void cback_propagate(std::vector<CNode*> &search_path, tools::CMinMaxStats &min_max_stats, int to_play, float value, float discount);
     void cbatch_back_propagate(int hidden_state_index_x, float discount, const std::vector<float> &value_prefixs, const std::vector<float> &values, const std::vector<std::vector<float>> &policies, tools::CMinMaxStatsList *min_max_stats_lst, CSearchResults &results, std::vector<int> is_reset_lst);
-    int cselect_child(CNode* root, tools::CMinMaxStats &min_max_stats, int pb_c_base, float pb_c_init, float discount, float mean_q);
-    float cucb_score(CNode *child, tools::CMinMaxStats &min_max_stats, float parent_mean_q, int is_reset, float total_children_visit_counts, float parent_value_prefix, float pb_c_base, float pb_c_init, float discount);
-    void cbatch_traverse(CRoots *roots, int pb_c_base, float pb_c_init, float discount, tools::CMinMaxStatsList *min_max_stats_lst, CSearchResults &results);
+    int cselect_child(CNode* root, tools::CMinMaxStats &min_max_stats, float pb_c_1, float pb_c_2, float pb_c_3, float discount, float mean_q, float mean_w);
+    float cucb_score(CNode *child, tools::CMinMaxStats &min_max_stats, float parent_mean_q, float parent_mean_w, int is_reset, float total_children_visit_counts, float parent_value_prefix, float pb_c_1, float pb_c_2, float pb_c_3, float discount);
+    void cbatch_traverse(CRoots *roots, float pb_c_1, float pb_c_2, float pb_c_3, float discount, tools::CMinMaxStatsList *min_max_stats_lst, CSearchResults &results);
 }
 
 #endif
