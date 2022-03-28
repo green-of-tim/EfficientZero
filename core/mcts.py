e8d0013d9b932e9ae37bf5393ff332397083c5a3
green-of-tim <62169271+green-of-tim@users.noreply.github.com>
2022-03-29 00:16:26 +0300

    Update mcts.py

--- core/mcts.py
+++ core/mcts.py
@@ -28,6 +28,7 @@ class MCTS(object):
             num = roots.num
             device = self.config.device
             pb_c_1, pb_c_2, pb_c_3, discount = self.config.pb_c_1, self.config.pb_c_2, self.config.pb_c_3, self.config.discount
+            mode = self.config.mode
             # the data storage of hidden states: storing the states of all the tree nodes
             hidden_state_pool = [hidden_state_roots]
             # 1 x batch x 64
